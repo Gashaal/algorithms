6d627b71cc5b0739daee9559e404522c1120e679
Roman Dyachenko <antinaar@gmail.com>
2020-04-13 20:40:18 +0300

    add fizzbuzz

---
+++ fizzbuzz/fizzBuzz.spec.js
@@ -0,0 +1,22 @@
+const test = require("ava");
+const fizzBuzz = require("./fizzBuzz");
+
+test('fizzBuzz', t => {
+    t.is(fizzBuzz(15), [
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz"
+    ]);
+});
