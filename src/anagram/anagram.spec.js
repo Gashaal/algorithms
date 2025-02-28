fb130dca7b630846f1288a34bd8267f0a930097a
Roman Dyachenko <antinaar@gmail.com>
2025-02-28 10:15:34 +0300

    add leetcode problem number

--- src/anagram/anagram.spec.js
+++
@@ -1,7 +0,0 @@
-const test = require("ava");
-const isAnagram = require('./anagram');
-
-test('isAnagram', t => {
-    t.is(isAnagram("anagram", "nagaram"), true);
-    t.is(isAnagram("rat", "car"), false);
-});
